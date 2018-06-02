9856ddf74294364c57db41c0e58072d786ca3060
HalimaAbdalla <halima.cfk@gmail.com>
2018-06-02 15:56:54 +0300

    data structures

---
+++ assign.py
@@ -0,0 +1,7 @@
+mylist = [6,10,11,30]
+
+mylist1 = [6,"Kartele","Cacy",10,11,"Mamu","Khadice",30]
+
+mydict = {"halima":"10","kartele":"6","khadice":"30","mamu":"11"}
+
+print(mylist,mylist1,mydict)
